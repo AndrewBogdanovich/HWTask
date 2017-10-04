85b9955bd42e8a77c3d608f3804599a20981eadb
AndrewBogdanovich <htconev228@gmail.com>
2017-10-04 12:45:14 +0300

    edit MainActivity

--- app/src/main/java/com/andrewbogdanovich/github/hwtask/MainActivity.java
+++ app/src/main/java/com/andrewbogdanovich/github/hwtask/MainActivity.java
@@ -11,4 +11,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //add some info to master brunch
     }
+    public void methodTwo()
+    {
+        //some info
+    }
 }
